13cd656f01636b0090f014ccd80ffd424694f28d
heartleth <myjmyjmyj237@gmail.com>
2021-02-22 16:14:49 +0900

    class tracking
    ㅇㅇ

--- extension.ts
+++ extension.ts
@@ -27,6 +27,113 @@ function indent_size(defs: (number|string)[][]): indent|undefined {
     }
 }
 
+function trace_fn_info(defs: (number|string)[][], name: string, doc: vscode.TextDocument): string {
+    const infos = defs.filter(e=>RegExp(`^[^;]*when [a-zA-Z0-9_]+ ${name.slice(0, name.length-1)}`, 'i').test(e[0]+''));
+    return infos.map(e=>{
+        const infos = (e[0]+'').trim().replace(/,/g, ' , ').split(/\s+/).slice(2);
+        let args = [];
+        let sess = '';
+        for (const i of args) {
+            if (i==',') {
+                args.push(sess.split(' '));
+                sess = '';
+            }
+            else if (i=='returns') {
+                if (sess.length > 0) args.push(sess.split(' '));
+                sess = '';
+            }
+            else {
+                sess += i + ' ';
+            }
+        }
+        return `In ${file_format(Number(e[1]), doc)}\n\n    ${e[0]}\n\n`;
+    }).join('');
+}
+
+function trace(idx: number, ni: any, sp: string[]): ObjectTree {
+    let ret = ni;
+    if (sp[idx-1]=='having') {
+        ret = (trace(idx-2, { tp: 'obj', name: sp[idx-2], child: ret }, sp));
+    }
+    if (sp[idx+1]=='in') {
+        ret = (trace(idx+2, { tp: 'ns', name: sp[idx+2], child: ret }, sp));
+    }
+    return ret;
+};
+
+function stringify(e: ObjectTree): string {
+    if (e.tp == 'root') {
+        return `${e.name}`;
+    }
+    if (e.tp == 'ns') {
+        return `namespace ${e.name} { ${stringify(e.child)} }`;
+    }
+    if (e.tp == 'obj') {
+        return `${e.name} { ${stringify(e.child)} }`;
+    }
+};
+
+function get_var_context(name: string, s: number, context: string[], doc: vscode.TextDocument, pos: vscode.Position, defs: (string|number)[][]): (string|number)[][] {
+    const trace_object = trace(s, {tp:'root', name}, context);
+
+    if (trace_object.tp == 'root') {
+        const variables = defs.filter(e=>e[1]<=pos.line).filter(e=>RegExp(`^[^;]*(let|have|make) ([a-zA-Z0-9_]+ )*${name}( (is|as|:|->|with|for|of|about|to).*|(;.*)?)$`, 'i').test(e[0]+''));
+        return variables;
+    }
+    else {
+        let indent = indent_size(defs);
+        let findcontext = defs;
+        let onion = trace_object;
+        while (onion.tp == 'ns') {
+            let line = findcontext.filter(e=>RegExp(`^[^;]*name( )?space ${onion.name}$`, 'i').test(e[0]+''))[0];
+            let lnnumber: number = Number(line[1]);
+            let line_indents = String(line[0]).search(/[a-zA-Z0-9\[\](){}_]/)/indent.howmuch;
+            let trimmed = findcontext.filter(e=>e[1]>lnnumber);
+            findcontext = [];
+            for (const s of trimmed) {
+                findcontext.push(s);
+                if (!RegExp(`^${indent.char.repeat(indent.howmuch*(line_indents+1))}`).test(s[0]+'')) break;
+            }
+            onion = onion.child;
+        }
+
+        if (onion.tp == 'root') {
+            const variables = findcontext.filter(e=>RegExp(`^[^;]*(let|have|make) ([a-zA-Z0-9_]+ )*${onion.name}( (is|as|:|->|with|for|of|about|to).*|(\s*;.*)?)?$`, 'i').test(e[0]+''));
+            return variables;
+        }
+        else if (onion.tp == 'obj') {
+            let variables = findcontext.filter(e=>RegExp(`^[^;]*(let|have|make) ([a-zA-Z0-9_]+ )*${onion.name}( (is|as|:|->|with|for|of|about|to).*|(\s*;.*)?)?$`, 'i').test(e[0]+''));
+            let statement = variables[variables.length-1][0] + '';
+            let typename = statement.match(RegExp(`[a-z0-9A-Z_]+(?=( ${onion.name}))`))[0];
+            if (/^(make|let|have)$/i.test(typename)) {
+                return variables;
+            }
+            while (onion.tp == 'obj') {
+                let line = defs.filter(e=>RegExp(`^[^;]*class [a-zA-Z0-9_]+ type ${typename}$`, 'i').test(e[0]+''));
+                let lnnumber: number = Number(line[0][1]);
+                let line_indents = String(line[0]).search(/[a-zA-Z0-9\[\](){}_]/)/indent.howmuch;
+                let trimmed = defs.filter(e=>e[1]>lnnumber);
+                findcontext = [];
+                for (const s of trimmed) {
+                    findcontext.push(s);
+                    if (!RegExp(`^${indent.char.repeat(indent.howmuch*(line_indents+1))}`).test(s[0]+'')) break;
+                }
+                onion = onion.child;
+                variables = findcontext.filter(e=>RegExp(`^[^;]*(has) ([a-zA-Z0-9_]+ )*${onion.name}( (is|as|:|->|with|for|of|about|to).*|(\s*;.*)?)?$`, 'i').test(e[0]+''));
+                statement = variables[variables.length-1][0] + '';
+                typename = statement.match(RegExp(`[a-z0-9A-Z_]+(?=( ${onion.name}))`))[0];
+                if (/^(has)$/i.test(typename)) {
+                    return variables;
+                }
+                else if (onion.tp == 'root') return variables;
+            }
+        }
+    }
+}
+function get_type(name: string, context: string[], defs: (string|number)[][]) {
+    
+}
+
 function definition(context: string, doc: vscode.TextDocument, pos: vscode.Position): string {
     if (/^[\s\t\r\n]$/.test(doc.getText(new vscode.Range(pos, new vscode.Position(pos.line, pos.character+1))))) return '';
     if (context.trim()[0] == ';') return '';
@@ -36,32 +143,13 @@ function definition(context: string, doc: vscode.TextDocument, pos: vscode.Posit
     const trimmed = context.trim();
 
     if (/^[a-z]+[:!=]$/i.test(name)) {
-        const infos = defs.filter(e=>RegExp(`^when [a-z]+ ${name.slice(0, name.length-1)}`, 'i').test(e[0]+''));
-        return infos.map(e=>{
-            const infos = (e[0]+'').trim().replace(/,/g, ' , ').split(/\s+/).slice(2);
-            let args = [];
-            let sess = '';
-            for (const i of args) {
-                if (i==',') {
-                    args.push(sess.split(' '));
-                    sess = '';
-                }
-                else if (i=='returns') {
-                    if (sess.length > 0) args.push(sess.split(' '));
-                    sess = '';
-                }
-                else {
-                    sess += i + ' ';
-                }
-            }
-            return `In ${file_format(Number(e[1]), doc)}\n\n    ${e[0]}\n\n`;
-        }).join('');
+        return trace_fn_info(defs, name, doc);
     }
     else {
         if (/^(make|let|have|it|when|for|of|do|was|were|repeat|while|if|else|lib(rary)?|import|const(ant)?|static|true|false|plus|minus|and(and)?|or(or)?)$/i.test(name)) return `Keyword: \`${name}\``;
         if (/^(to|for|of|->|:|about|with)$/i.test(name)) return `Prep.: \`${name}\``;
-        if (new RegExp(`^[^;]*(let|have|make) ([a-z]+ )*${name} [a-z]+( (is|as|:|->|with|for|of|about|to).*|(;.*)?)$`, 'i').test(context)) {
-            const classes = defs.filter(e=>RegExp(`^class [a-z]+ type ${name}`, 'i').test(e[0]+''))[0];
+        if (new RegExp(`^[^;]*(let|have|make) ([a-zA-Z0-9_]+ )*${name} [a-zA-Z0-9_]+( (is|as|:|->|with|for|of|about|to).*|(;.*)?)$`, 'i').test(context)) {
+            const classes = defs.filter(e=>RegExp(`^class [a-zA-Z0-9_]+ type ${name}`, 'i').test(e[0]+''))[0];
             return `In ${file_format(Number(classes[1]), doc)}\n\n    ${classes[0]}\n\n`;
         }
         else {
@@ -75,59 +163,19 @@ function definition(context: string, doc: vscode.TextDocument, pos: vscode.Posit
                 }
                 s += 1 + sp[i].length;
             }
+
+            if (/^(to|do)$/i.test(sp[s-1])) {
+                return trace_fn_info(defs, name, doc);
+            }
+            if (/^(was|were)$/i.test(sp[s+1])) {
+                return trace_fn_info(defs, name, doc);
+            }
+            if (/^(->|with|for|:|of|about|to)$/i.test(sp[s+1])) {
+                return trace_fn_info(defs, name, doc);
+            }
             
-            const trace = (idx: number, ni: any): ObjectTree => {
-                let ret = ni;
-                if (sp[idx-1]=='having') {
-                    ret = (trace(idx-2, { tp: 'obj', name: sp[idx-2], child: ret }));
-                }
-                if (sp[idx+1]=='in') {
-                    ret = (trace(idx+2, { tp: 'ns', name: sp[idx+2], child: ret }));
-                }
-                return ret;
-            };
-
-            const stringify = (e: ObjectTree): string => {
-                if (e.tp == 'root') {
-                    return `${e.name}`;
-                }
-                if (e.tp == 'ns') {
-                    return `namespace ${e.name} { ${stringify(e.child)} }`;
-                }
-                if (e.tp == 'obj') {
-                    return `${e.name} { ${stringify(e.child)} }`;
-                }
-            };
-            const trace_object = trace(s, {tp:'root', name});
-
-            if (trace_object.tp == 'root') {
-                const variables = defs.filter(e=>e[1]<=pos.line).filter(e=>RegExp(`^[^;]*(let|have|make) ([a-z]+ )*${name}( (is|as|:|->|with|for|of|about|to).*|(;.*)?)$`, 'i').test(e[0]+'')).map(e=>`In ${file_format(Number(e[1]), doc)}\n\n    ${e[0].toString().trim()}\n\n`);
-                return variables[variables.length-1];
-            }
-            else {
-                let indent = indent_size(defs);
-                let findcontext = defs;
-                let onion = trace_object;
-                let idts = 0;
-                while (onion.tp == 'ns') {
-                    let line = findcontext.filter(e=>RegExp(`^${indent.char.repeat(indent.howmuch*(idts))}name( )?space ${onion.name}$`, 'i').test(e[0]+''))[0];
-                    let lnnumber: number = Number(line[1]);
-                    let line_indents = String(line[0]).search(/[a-zA-Z0-9\[\](){}_]/)/indent.howmuch;
-                    let trimmed = findcontext.filter(e=>e[1]>lnnumber);
-                    findcontext = [];
-                    for (const s of trimmed) {
-                        findcontext.push(s);
-                        if (!RegExp(`^${indent.char.repeat(indent.howmuch*(line_indents+1))}`).test(s[0]+'')) break;
-                    }
-                    onion = onion.child;
-                    idts++;
-                }
-                if (onion.tp == 'root') {
-                    const variables = findcontext.filter(e=>RegExp(`^[^;]*(let|have|make) ([a-z]+ )*${name}( (is|as|:|->|with|for|of|about|to).*|(;.*)?)$`, 'i').test(e[0]+'')).map(e=>`In ${file_format(Number(e[1]), doc)}\n\n    ${e[0].toString().trim()}\n\n`);
-                    return variables[variables.length-1];
-                }
-                return findcontext.map(e=>String(e[0])).join('\n');
-            }
+            let candidates = get_var_context(name, s, sp, doc, pos, defs).map(e=>`In ${file_format(Number(e[1]), doc)}\n\n    ${e[0].toString().trim()}\n\n`);
+            return candidates.join('');
         }
     }
 }
